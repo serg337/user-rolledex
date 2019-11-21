de8ea762e144b09d4b1aca947a69c40acb70f3ca
Sergio Valencia <serg337@gmail.com>
2019-11-21 11:45:56 +0100

    adding files to git hub pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import uuid from "uuid/v4";
+
 import "./App.css";
+import { CardList } from "./components/card-list/CardList";
+import { SearchBox } from "./components/search-box/SearchBox";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -17,12 +20,24 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
+
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={uuid()}>{monster.name}</h1>
-        ))}
+        <h1>Monster Rolledex</h1>
+        <SearchBox
+          handleChange={this.handleChange}
+          placeholder="search monsters"
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
